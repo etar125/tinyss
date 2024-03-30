e7798f68a295d1f934571361a74175ae3d2c4e0b
etar125 <inniesharpsoftware@gmail.com>
2024-03-30 17:25:22 +0300

    исправления

--- lexer.hpp
+++ lexer.hpp
@@ -15,12 +15,14 @@ vecstr split(std::string str, char delim)
 		else { result.push_back(temp); temp = ""; } 
 	}
 	result.push_back(temp); temp = "";
+	return result;
 }
 
 std::string trip(std::string str)
 {
+
 	int i = 0;
-	while(str[i] != ' ' || str[i] != '\t') i++;
+	while(str[i] == ' ' || str[i] == '\t') i++;
 	return str.substr(i);
 }
 
@@ -28,18 +30,19 @@ std::vector<tkn> Lexer(vecstr code)
 {
 	std::vector<tkn> result;
 	vecstr res;
+	bool com = true;
 	for(std::string s : code)
 	{
 		res = split(trip(s), ' ');
-		bool com = true;
 		for(int i = 0; i < res.size(); i++)
 		{
 			if(res[i][0] == ';') break;
-			else if(res[i][0] == '$') result.push_back(tkn(tkntp::val, res[i].substr(1)));
+			else if(res[i][0] == '$') result.push_back(tkn(tkntp::var, res[i].substr(1)));
 			else if(res[i][0] == ':') result.push_back(tkn(tkntp::lab, res[i].substr(1)));
-			else if(com) result.push_back(tkn(tkntp::com, res[i]));
+			else if(com) { result.push_back(tkn(tkntp::com, res[i])); com = false; }
 			else result.push_back(tkn(tkntp::val, res[i]));
 		}
+		com = true;
 	}
-	return 0;
+	return result;
 }
