2b51d4c7b98ec49e6e980bedd49d1e4127c69373
etar125 <inniesharpsoftware@gmail.com>
2024-03-30 22:51:06 +0300

    комментарии

--- test.cpp
+++ test.cpp
@@ -9,9 +9,9 @@ tss a;
 
 void run()
 {
-	std::vector<std::string> code =
+	std::vector<std::string> code = // наш код
 	{
-		"define test 26",
+		"define test 25",
 		"define not 126",
 		"if test e 25",
 		"op test * 5",
@@ -22,23 +22,26 @@ void run()
 		"gcall testcout",
 		"end"
 	};
-	TSSException b = a.docode(code);
-	// std::cout << "Index: " << std::to_string(b.index);
-	// switch(b.token.type)
-	// {
-	// 	case tkntp::com: std::cout << " Type: Command "; break;
-	// 	case tkntp::var: std::cout << " Type: Variable "; break;
-	// 	case tkntp::val: std::cout << " Type: Value "; break;
-	// 	case tkntp::lab: std::cout << " Type: Label "; break;
-	// }
-	// std::cout << "Value: " << b.token.val << std::endl;
+	TSSException b = a.docode(code); // выполняем код
+	/* Данный код нужен для отладки
+	std::cout << "Index: " << std::to_string(b.index);
+	switch(b.token.type)
+	{
+		case tkntp::com: std::cout << " Type: Command "; break;
+		case tkntp::var: std::cout << " Type: Variable "; break;
+		case tkntp::val: std::cout << " Type: Value "; break;
+		case tkntp::lab: std::cout << " Type: Label "; break;
+	}
+	std::cout << "Value: " << b.token.val << std::endl;
+	*/
 }
 
 int main()
 {
 	std::thread ads(run);
-	ads.detach();
-	a.work = true;
+	ads.detach(); // Отсоединяем TSS от текущего потока, чтобы проверять функции
+	a.work = true; // ставим, что программа работает
+	// Пока программа работает, проверяем вызвана ли функция, если да, то проверяем имя и выполняем нужный код
 	while(a.work) { if(a.called) { if (a.gcall == "testcout") { std::cout << "etar" << a.stack[0] << std::endl; a.called = false; } } }
 	return 0;
 }
