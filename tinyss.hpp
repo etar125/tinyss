530738c2ab650f9e8038938f90d58dfce8b85b9f
etar125 <inniesharpsoftware@gmail.com>
2024-03-30 17:24:16 +0300

    исправленный заголовок

--- tinyss.hpp
+++ tinyss.hpp
@@ -1,26 +1,28 @@
+#pragma once
+
 #include <string>
 #include <vector>
 
-struct var
+struct varb
 {
-	char name[5];
+	std::string name;
 	std::string val;
 
-	var();
-	var(char _name[5], std::string _val);
+	varb();
+	varb(std::string _name, std::string _val);
 };
 
 struct tss
 {
-	std::vector<var> vars;
-	std::vector<std::string> gfuncs;
+	std::vector<varb> vars;
 	std::vector<std::string> stack;
-	bool called;
-	std::string gcall;
 
-	int find(char name[5]);
-	void set(char name[5], std::string val);
-	std::string get(char name[5]);
-	void del(char name[5]);
+	int find(std::string name);
+	void set(std::string name, std::string val);
+	std::string get(std::string name);
+	void del(std::string name);
+	void gfunc(std::string name);
 	int docode(std::vector<std::string> code);
+
+	tss();
 };
