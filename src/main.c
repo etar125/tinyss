77023904c2c7506796ad4de06350aa35dd9e414f
etar125 <inniesharpsoftware@gmail.com>
2025-02-02 15:48:51 +0300

    Другой способ деления на аргументы

--- src/main.c
+++ src/main.c
@@ -30,30 +30,83 @@ bool tss_strcmp(char *data1, size_t size1, char *data2, size_t size2) {
     } return true;
 }
 
+char schar(char ch) {
+    return ch; 
+    if(ch == 'n') {
+        return '\n';
+    } else if(ch == 't') {
+        return '\t';
+    } else if(ch == 'a') {
+        return '\033';
+    } return ch;
+}
+
+typedef struct {
+    char *data;
+    size_t size, pos;
+} tss_arg;
+void tss_ainit(tss_arg *a) {
+    if(a == NULL) { return; }
+    a->data = NULL;
+    a->size = 0;
+    a->pos = 0;
+} void tss_aadd(tss_arg *a, char ch) {
+    if(a == NULL) { return; }
+    if(a->data == NULL) {
+        a->size = 8;
+        a->data = malloc(8);
+        a->data[a->pos++] = ch;
+        return;
+    }
+    if(a->pos == a->size) {
+        char *old = a->data;
+        size_t prev = a->size;
+        a->size += prev / 4;
+        a->data = malloc(a->size);
+        memcpy(a->data, old, prev);
+        free(old);
+    } a->data[a->pos++] = ch;
+} char* tss_aget(tss_arg *a) {
+    if(a == NULL) { return NULL; }
+    if(a->pos == a->size) {
+        char *old = a->data;
+        size_t prev = a->size;
+        a->size += 1;
+        a->data = malloc(a->size);
+        memcpy(a->data, old, prev);
+        free(old);
+    } a->data[a->pos++] = '\0';
+    return a->data;
+}
+
 tss_exception tss_docode(tss_varlist *list, char *code, size_t size) {
-    uint8_t argc = 1;
-    size_t argpos[6], line = 0, psize;
+    uint8_t argc = 0;
+    size_t line = 0, psize;
     char *arg, *tmp;
+    tss_arg args[5];
+    for(uint8_t i = 0; i < 5; i++) {
+        tss_ainit(&args[i]);
+    }
+    bool com = false;
     tss_stack st;
     tss_sinit(&st);
     tss_exception ret;
     ret.code = 0;
-    argpos[0] = 0;
     for(size_t i = 0; i < size; i++) {
         if(code[i] == '\n') {
-            argpos[argc] = i + 1;
+            // argpos[argc] = i;
             line++;
-            arg = &code[argpos[0]];
-            psize = argpos[1] - argpos[0] - 1;
+            arg = tss_aget(&args[0]);
+            psize = strlen(arg);
             if(tss_strcmp(arg, psize, "nop", 3)) { }
             else if(tss_strcmp(arg, psize, "gpushb", 6)) {
-                if(argc != 2) {
+                if(argc != 1) {
                     _retset;
                     ret.code = argc < 2 ? 3 : 2;
                     return ret;
                 }
-                arg = &code[argpos[1]];
-                psize = argpos[2] - argpos[1] - 1;
+                arg = tss_aget(&args[1]);
+                psize = strlen(arg);
                 if(psize == 0 || (psize == 1 && arg[0] == '$')) {
                     _retset;
                     ret.code = 4;
@@ -76,13 +129,13 @@ tss_exception tss_docode(tss_varlist *list, char *code, size_t size) {
                     free(tmp);
                 }
             } else if(tss_strcmp(arg, psize, "gcall", 5)) {
-                if(argc != 2) {
+                if(argc != 1) {
                     _retset;
                     ret.code = argc < 2 ? 3 : 2;
                     return ret;
                 }
-                arg = &code[argpos[1]];
-                psize = argpos[2] - argpos[1] - 1;
+                arg = tss_aget(&args[1]);
+                psize = args[1].size;
                 if(psize == 0 || arg[0] == '$') {
                     _retset;
                     ret.code = 4;
@@ -96,24 +149,29 @@ tss_exception tss_docode(tss_varlist *list, char *code, size_t size) {
                 free(tmp);
             }
 
-            argc=1;
-            argpos[0] = i + 1;
-        } else if(code[i] == ' ' || code[i] == '\t') {
-            if(argc == 1 && (argpos[0] - i) == 0) {
-                argpos[0] = i + 1;
-            } else if(argc < 5) {
-                argpos[argc] = i + 1;
-                argc++;
-            } else {
-                _retset;
-                ret.code = 2;
-                return ret;
+            argc=0;
+            for(uint8_t i = 0; i < 5; i++) {
+                if(args[i].data != NULL) { free(args[i].data); }
+                tss_ainit(&args[i]);
             }
-        } else if(code[i] == ':' && argc == 1 && (argpos[0] - i) == 0) {
+        } else if((code[i] == '"' || code[i] == '\'') && (i != 0 && code[i - 1] != '\\')) {
+            if(com) { com = false; }
+            else { com = true; }
+        } else if(code[i] == ' ' || code[i] == '\t') {
+            if(argc == 0 && args[0].data == NULL) {
+                
+            } else if(!com) {
+                if(argc < 5) {
+                    argc++;
+                } else {
+                    _retset;
+                    ret.code = 2;
+                    return ret;
+                }
+            } else { tss_aadd(&args[argc], schar(code[i])); }
+        } else if(code[i] == ':' && argc == 0 && args[0].data == NULL) {
             while(code[i] != '\n') { i++; }
-            argc=1;
-            argpos[0] = i + 1;
             continue;
-        }
+        } else { tss_aadd(&args[argc], schar(code[i])); }
     } return ret;
 }
