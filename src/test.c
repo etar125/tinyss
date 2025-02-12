3e603f6dc760c373800dbc7f7639d1cc59f0e83d
etar125 <inniesharpsoftware@gmail.com>
2025-02-12 18:43:52 +0300

    Вычисление простого примера в тесте

--- src/test.c
+++ src/test.c
@@ -4,16 +4,28 @@
 #include <malloc.h>
 
 void tss_gfunc(tss_varlist *list, tss_stack *stack, char *name) {
-    if(strcmp(name, "print") == 0) {
+    if(strcmp(name, "printline") == 0) {
         printf("%s\n", tss_pop(stack));
+    } else if(strcmp(name, "print") == 0) {
+        printf("%s", tss_pop(stack));
     }
 }
 
 int main(void) {
-    char code[] = "\t  nop\n  :\tx\ndefine var \"hello из \\\\\\\"tinyss\\\\\\''\n  gpushb\t$var\n gcall\tprint\n";
+    char code[] =
+    ":rem main\n\
+    :main\n\
+    \tdefine a 2\n\
+        define\tb 512\n\
+    op a ^ 10\n\
+    op b / -256\n\
+    op a / $b\n\
+    gpushb \"2^10 / (512 / -256) = '\n\
+    gcall print\n\
+    gpushb $a\n\
+    gcall printline";
     tss_varlist list;
     tss_vlinit(&list);
     tss_printerr(tss_docode(&list, code, strlen(code)));
-    printf("%s\n", tss_getvar(&list, "var"));
     return 0;
 }
